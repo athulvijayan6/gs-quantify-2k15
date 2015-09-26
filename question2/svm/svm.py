19b7a69eb2c7799eee10ebf1b83460124caece63
Athul <athulvijayan6@gmail.com>
2015-09-26 06:32:17 +0530

    svm done

---
+++ question2/svm/svm.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Athul
+# @Date:   2015-09-26 02:16:55
+# @Last Modified by:   Athul
+# @Last Modified time: 2015-09-26 06:26:46
+from __future__ import division
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import scipy.stats
+plt.style.use('ggplot')
+
+trainData = np.load('../data/Initial_Training_Data.npy')
+np.random.shuffle(trainData)
+testData = trainData[int(trainData.shape[0]*0.8):,:]
+trainData = trainData[:int(trainData.shape[0]*0.8),:]
+# testData = np.load('../data/Initial_Test_Data.npy')
+truth = trainData[:, 19].astype(int)
+
+# No need to remove NaNs
+trainFeatures = np.hstack((trainData[:, 1:19], trainData[:, 20:]))
+testFeatures = np.hstack((testData[:, 1:19], testData[:, 20:]))
+
+# libsvm filenames
+svm_train_data = 'results/svm_train_data'
+svm_test_data = 'results/svm_test_data'
+modelfile = 'results/svm_model_data_01.model'
+resultFile = 'results/iter_01'
+with open(svm_train_data, 'w') as f:
+    for i in xrange(trainFeatures.shape[0]):
+        d = {}
+        for j in xrange(trainFeatures[i].size):
+            if not np.isnan(trainFeatures[i, j]):
+                d[j+1] = trainFeatures[i, j]
+        line = str(truth[i]+1)
+        for index, value in d.iteritems():
+            line += ' '+str(index)+':'+str(value)
+        line += '\n'
+        f.write(line)
+    f.close()
+
+with open(svm_test_data, 'w') as f:
+    for i in xrange(testFeatures.shape[0]):
+        d = {}
+        for j in xrange(testFeatures[i].size):
+            if not np.isnan(testFeatures[i, j]):
+                d[j+1] = testFeatures[i, j]
+        line = str(int(testData[i, 19])+1)
+        for index, value in d.iteritems():
+            line += ' '+str(index)+':'+str(value)
+        line += '\n'
+        f.write(line)
+    f.close()
+
+# ============================= Train SVM ====================
+print('scaling data')
+command = './libsvm/svm-scale -l -1 -u 1 -s results/range {0} > {1}.scale'
+command = command.format(svm_train_data, svm_train_data)
+os.system(command)
+
+command = './libsvm/svm-scale -r results/range {0} > {1}.scale'
+command = command.format(svm_test_data, svm_test_data)
+os.system(command)
+
+svm_train_data += '.scale'
+svm_test_data += '.scale'
+
+
+print('starting svm training')
+s = 0
+t = 2
+d = 3
+# g gamma : set gamma in kernel function (default 1/k)
+r = 0
+c = 1
+b = 0
+v = 5
+command = './libsvm/svm-train {7} {8} -s {0} -t {1} -d {2} -r {3} -c {4} -b {5} -v {6}'
+command = command.format(s, t, d, r, c, b, v, svm_train_data, modelfile)
+os.system(command)
+
+# # ============================== Test svm model===============
+print('starting svm testing')
+b = 0
+command = './libsvm/svm-predict -b {0} {1} {2} {3}'
+command = command.format(b, svm_test_data, modelfile, resultFile)
+os.system(command)
+
+
--- question2/svm/svm_scipy.py
+++ question2/svm/svm_scipy.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Author: Athul Vijayan
-# @Date:   2015-09-25 22:38:46
+# @Author: Athul
+# @Date:   2015-09-26 06:06:47
 # @Last Modified by:   Athul
-# @Last Modified time: 2015-09-26 02:20:54
+# @Last Modified time: 2015-09-26 06:26:30
 from __future__ import division
 import pandas as pd
 import numpy as np
+import os
+from sklearn import svm
 import matplotlib.pyplot as plt
-from sklearn import mixture
 import scipy.stats
 plt.style.use('ggplot')
 
@@ -17,7 +18,7 @@ np.random.shuffle(trainData)
 testData = trainData[int(trainData.shape[0]*0.8):,:]
 trainData = trainData[:int(trainData.shape[0]*0.8),:]
 # testData = np.load('../data/Initial_Test_Data.npy')
-truth = trainData[:, 19]
+truth = trainData[:, 19].astype(int)
 
 # Remove NaNs from traindata
 col_mean = scipy.stats.nanmean(trainData, axis=0)
@@ -29,27 +30,19 @@ col_mean = scipy.stats.nanmean(testData, axis=0)
 ids = np.where(np.isnan(testData))
 testData[ids] = np.take(col_mean, ids[1]) 
 
-#  ============================= Train GMM =======================
-numClasses = int(max(truth) - min(truth) + 1)
-models = []
-for cls in xrange(numClasses):
-    classData = trainData[trainData[:, 19].astype(int) == cls]
-    numGaussians = 28
-    features = np.hstack((classData[:, 1:19], classData[:, 20:]))
-    models.append(mixture.GMM(n_components=numGaussians))
-    models[cls].fit(features)
-
-# ====================== test GMM ===========================
+trainFeatures = np.hstack((trainData[:, 1:19], trainData[:, 20:]))
 testFeatures = np.hstack((testData[:, 1:19], testData[:, 20:]))
-scores = np.zeros((testData.shape[0], numClasses))
-for cls in xrange(numClasses):
-    sc = models[cls].score(testFeatures)
-    scores[:, cls] = sc
-targetScores = np.amax(scores, axis = 1)
-targetClass = np.argmax(scores, axis = 1)
+
+# ============================= Train SVM ====================
+print('starting svm training')
+model = svm.SVC()
+model.fit(trainFeatures, truth)
+# # ============================== Test svm model===============
+print('starting svm testing')
+targetClass = model.predict(testFeatures)
 trueClass = testData[:, 19].astype(int)
 
 miss = np.count_nonzero(trueClass - targetClass)
-accuracy = 1-(miss/testData.shape[0])
+accuracy = model.score(testFeatures, trueClass)
 print('Number of misses '+str(miss)+' in '+str(testData.shape[0])+' trials')
 print('Accuracy = '+str(accuracy))
